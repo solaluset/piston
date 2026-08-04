ae7e51692c1f703eef53ab78fbf4275e412afd2d
solaluset <60041069+solaluset@users.noreply.github.com>
2026-08-05 02:59:48 +0300

    Put YABI package into Python

--- packages/python/3.14.6/build.sh
+++ packages/python/3.14.6/build.sh
@@ -10,7 +10,9 @@ curl "https://www.python.org/ftp/python/3.14.6/Python-3.14.6.tgz" -o python.tar.
 tar xzf python.tar.gz --strip-components=1
 rm python.tar.gz
 
-./configure --prefix "$PREFIX" --with-ensurepip=install --enable-optimizations --with-lto
+./configure --prefix "$PREFIX" --with-ensurepip=install \
+    --enable-optimizations --with-lto \
+    --disable-test-modules --without-static-libpython
 make -j$(nproc)
 make install -j$(nproc)
 
